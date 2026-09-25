43e01d8526bb27be91d4d122abf2bd29c131b058
r4jrandy <rkuturu@gmail.com>
2026-09-26 01:05:37 +0530

    Updated test case

--- tests/e2e/e2e.spec.ts
+++ tests/e2e/e2e.spec.ts
@@ -3,6 +3,7 @@ import HomePage from "../page-objects/home.page";
 import { log } from "../helpers/logger";
 
 test.describe("Way2Automation Login Test",{ tag: "@stage" }, () => {
+  test.skip(process.env.ENV_NAME === "dev", "Skipping test in dev environment");
   test("Way2Automation Login Test", async ({ page }, testInfo) => {
     // Get the config file
     const configFile = testInfo.project.use as any;
